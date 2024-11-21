b4e8c233e9f96b6219a59cba352ac710965a65a8
Alanna DEVLIN GENIN <alannadevlingenin@gmail.com>
2024-11-21 17:49:50 +0100

    Reformat cashier class

--- TP2/restaurants/cashier.py
+++ TP2/restaurants/cashier.py
@@ -2,8 +2,18 @@ from employee import Employee
 
 
 class Cashier(Employee):
-    def __init__(self, name, surname, registration_number, cash_register,):
-        super().__init__(name, surname, registration_number,)
+    def __init__(
+        self,
+        name,
+        surname,
+        registration_number,
+        cash_register,
+    ):
+        super().__init__(
+            name,
+            surname,
+            registration_number,
+        )
         self.cash_register = cash_register
 
     def handle_payment(self):
